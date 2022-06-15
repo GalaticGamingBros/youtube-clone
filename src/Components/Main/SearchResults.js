4ab4b21dcc9c7c95a745a659cbeaa4b20726d4b8
Abdullah-Tasfiek <atasfiek.ece@gmail.com>
2022-06-14 22:49:01 -0400

    building static version of the website

---
+++ src/Components/Main/SearchResults.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SearchResults() {
+  return (
+    <div>
+      <section>
+        <h2>No Search Results Yet!, Please submit a search above!</h2>
+      </section>
+    </div>
+  );
+}
+
+export default SearchResults;
