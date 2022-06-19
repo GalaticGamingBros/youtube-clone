8f3f8c726001f5555f3374ea7493f49372b1531f
Antonio Shivers <antonioshivers2500@yahoo.com>
2022-06-19 15:50:28 -0400

    fixing

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,21 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import YoutubeLogo from "../../Images/youtubeLogo.gif";
 
 const NavBar = () => {
-    return (
-        <header className="nav">
-            <strong id="youtube"> <img src={"https://cdn.dribbble.com/users/1923171/screenshots/5676763/media/fca46b74ba4f89fbb1569f860cb44916.gif"} width="200" height="200" alt="wait until the page loads" /> 
-            </strong>
-            <strong id="home">
-                <Link to="/home">Home</Link>
-            </strong>
+  return (
+    <header className="nav">
+      <strong id="youtube">
+        {" "}
+        <img
+          src={YoutubeLogo}
+          width="200"
+          height="200"
+          alt="wait until the page loads"
+        />
+      </strong>
+      <strong id="home">
+        <Link to="/">Home</Link>
+      </strong>
 
-            <strong id="about">
-                <Link to="/about">About</Link>
-            </strong>
-        </header>
-    );
+      <strong id="about">
+        <Link to="/about">About</Link>
+      </strong>
+    </header>
+  );
 };
 
 export default NavBar;
