086775d59c60be37e13f67ce7da5435c05c415b5
Abdullah-Tasfiek <atasfiek.ece@gmail.com>
2022-06-20 12:28:29 -0400

    added modal for comments section and updated styling

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -5,23 +5,21 @@ import YoutubeLogo from "../../Images/youtubeLogo.gif";
 
 const NavBar = () => {
   return (
-    <header className="nav">
-      <strong id="youtube">
-        {" "}
-        <img
-          src={YoutubeLogo}
-          width="200"
-          height="200"
-          alt="wait until the page loads"
-        />
-      </strong>
-      <strong id="home">
-        <Link to="/">Home</Link>
-      </strong>
+    <header className="nav" id="youtube">
+      <img
+        className="Youtube-logo"
+        src={YoutubeLogo}
+        width="150"
+        height="100"
+        alt="wait until the page loads"
+      />
+      <Link to="/" id="home">
+        Home
+      </Link>
 
-      <strong id="about">
-        <Link to="/about">About</Link>
-      </strong>
+      <Link to="/about" id="about">
+        About
+      </Link>
     </header>
   );
 };
